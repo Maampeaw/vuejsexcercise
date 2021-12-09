12d155c5729dca5314b3d53685f92408cbf1a0bf
Maampeaw <markamoah97@gmail.com>
2021-12-09 09:26:49 +0000

    Data binding and events in vue js

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template>
+<div>
+  <div class="navbar"></div>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <Mydata v-bind:users="users" />
+   <!-- <div v-for='user in userss' :key="user.name">
+     {{user.name}}
+     </div> -->
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Mydata from './components/Mydata.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Mydata
+  },
+  data(){
+    return{
+    users: [
+      {name:'Papa', color:'green', country:'Ghana', status:'online'},
+      {name:'Kwaku', color:'red', country:'Ghana', status:'online'},
+      {name:'Ruba', color:'purple', country:'Ghana', status:'online'},
+      {name:'David', color:'blue', country:'Ghana', status:'online'},
+      {name:'Mike', color:'black', country:'Ghana', status:'online'}
+    ]}
   }
 }
 </script>
 
 <style>
+*{
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +44,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.navbar{
+  height: 60px;
+  background-image: linear-gradient(to right top, #1a26b2, #b60076, #d33a3b, #c08421, #9bb85a);
 }
 </style>
---
+++ src/components/Mydata.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="data">
+      <h2>Users Online</h2>
+       
+       <div v-for='user in users' :key="user.name" v-show='showUsers'>
+           
+       <div class="profile">
+         <div class="profile-pic" :style='{background: user.color}'>{{user.name.slice(0,2)}}</div>
+         <div class="name">{{user.name}}</div>
+         <div class="shoesize">{{user.status}}</div>
+         <div class="status">{{user.country}}</div>
+     </div>
+
+
+       </div>
+
+       <button v-on:click='toggleShow'><span v-if='showUsers'>Hide users</span><span v-else>Show-users</span></button>
+       
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Mydata',
+    props: ["users"],
+    data(){
+        return{
+            showUsers : false,
+        }
+    },
+    methods:{
+        toggleShow(){
+            this.showUsers = !this.showUsers 
+        }
+    }
+}
+</script>
+
+<style>
+.profile{
+    display: flex;
+    justify-content: space-evenly;
+}
+.profile-pic{
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    color: white;
+    display: flex;
+    font-size: 16pt;
+    justify-content: center;
+    align-items: center;
+    font-weight: bolder;
+    opacity: 0.7;
+}
+div{
+    margin-bottom: 20px;
+}
+button{
+    width: 100px;
+    height: 40px;
+    border-radius: 8px;
+    background: white;
+    color: black
+
+}
+button:hover{
+    width: 105px;
+    height: 45px;
+    background: black;
+    color: white;
+}
+h2{
+    margin-bottom: 40px;
+}
+
+</style>
